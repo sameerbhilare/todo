07d30d5ab9e71720c62c0e1b64d02770c8b685d3
Sameer Bhilare <bhilare.sameer@gmail.com>
2023-09-16 14:36:19 +0530

    Initial setup

---
+++ src/main/java/com/husqvarna/todo/bl/TodoBlApplication.java
@@ -0,0 +1,13 @@
+package com.husqvarna.todo.bl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/husqvarna/todo/bl/TodoBlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husqvarna.todo.bl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
