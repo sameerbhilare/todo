9da3ecb366bc4d47f75f820448ee54ab14759a14
Sameer Bhilare <bhilare.sameer@gmail.com>
2023-09-18 13:10:54 +0530

    Added negative test cases and ordering

--- src/test/java/com/husqvarna/todo/bl/TodoBlApplicationTests.java
+++ src/test/java/com/husqvarna/todo/bl/TodoBlApplicationTests.java
@@ -1,7 +1,12 @@
 package com.husqvarna.todo.bl;
 
+import com.husqvarna.todo.bl.common.ErrorCode;
+import com.husqvarna.todo.bl.vo.ErrorVO;
 import com.husqvarna.todo.bl.vo.TodoVO;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,11 +22,12 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * Testing all the endpoints of the TODO_API.
+ * Integration Testing all the endpoints of the TODO_API.
  * We are using WebEnvironment.RANDOM_PORT approach instead of WebEnvironment.MOCK.
  * WebEnvironment.RANDOM_PORT approach is closer to test the real application. It is as if you test with a real http server.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TodoBlApplicationTests {
 
 	@Value(value="${local.server.port}")
@@ -30,21 +36,60 @@ class TodoBlApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	private static final String BASE_URL = "http://localhost:";
+	private static final String TODO_API = "/api/todos";
+	private static final String PATH_SEPARATOR = "/";
+
 	/**
 	 * Testing for GET /api/todos endpoint
 	 */
 	@Test
+	@Order(1)
 	public void getAllTodos() {
 
-		List<TodoVO> todos = this.restTemplate.getForObject("http://localhost:" + port + "/api/todos", List.class);
+		List<TodoVO> todos = this.restTemplate.getForObject(BASE_URL + port + TODO_API, List.class);
 		Assert.isTrue(todos.size() == 3, "Expected todos size=3. Actual=" + todos.size());
+	}
+
+	/**
+	 * Testing for GET /api/todos endpoint.
+	 * Passing valid status
+	 */
+	@Test
+	@Order(2)
+	public void getAllTodos_WithValidStatus() {
+
+		String validStatus = "ACTIVE"; // ACTIVE or COMPLETED
+		List<TodoVO> todos = this.restTemplate.getForObject(BASE_URL + port + TODO_API + "?" + "status=" + validStatus, List.class);
+		Assert.isTrue(todos.size() == 2, "Expected todos size=3. Actual=" + todos.size());
+	}
+
+
+	 /**
+	  * Testing for GET /api/todos endpoint.
+	  * NEGATIVE Test case: Passing invalid status
+	 */
+	@Test
+	@Order(3)
+	public void getAllTodos_WithInvalidStatus() {
+
+		String inValidStatus = "INVALID";
+		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(null, null);
+		ResponseEntity<ErrorVO> responseEntity = restTemplate.exchange(BASE_URL + port + TODO_API + "?" + "status=" + inValidStatus,
+				HttpMethod.GET, requestEntity, ErrorVO.class);
+
+		ErrorVO errorVo = responseEntity.getBody();
+
+		Assert.isTrue(ErrorCode.INVALID_STATUS.name().equals(errorVo.getErrorCode()), "Expected Todo name=" + ErrorCode.INVALID_STATUS.name() + ". Actual=" + errorVo.getErrorCode());
+
 	}
 
 	/**
 	 * Testing for GET /api/todos/{id} endpoint
 	 */
 	@Test
-	public void getTodo() {
+	@Order(4)
+	public void getTodo_Existing() {
 
 		// create new task to be saved
 		String expectedTodoName = "Test create todo";
@@ -54,21 +99,46 @@ class TodoBlApplicationTests {
 				.build();
 
 		// save the new task
-		TodoVO savedTodo = this.restTemplate.postForObject("http://localhost:" + port + "/api/todos", todoToBeSaved, TodoVO.class);
+		TodoVO savedTodo = this.restTemplate.postForObject(BASE_URL + port + TODO_API, todoToBeSaved, TodoVO.class);
 		Long expectedTodoId = savedTodo.getId();
 
 		// call GET api to fetch for one
-		TodoVO refetchedTodo = this.restTemplate.getForObject("http://localhost:" + port + "/api/todos/" + expectedTodoId, TodoVO.class);
+		TodoVO refetchedTodo = this.restTemplate.getForObject(BASE_URL + port + TODO_API + PATH_SEPARATOR + expectedTodoId, TodoVO.class);
 
 		Assert.isTrue(Objects.equals(refetchedTodo.getId(), expectedTodoId), "Expected Todo Id=" + expectedTodoId + ". Actual=" + expectedTodoId);
 		Assert.isTrue(refetchedTodo.getName().equals(expectedTodoName), "Expected Todo name=" + expectedTodoName + ". Actual=" + refetchedTodo.getName());
 		Assert.isTrue(refetchedTodo.getStatus().equals(expectedTodoStatus), "Expected Todo status=" + expectedTodoStatus + ". Actual=" + refetchedTodo.getStatus());
+
+		// delete after testing
+		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(savedTodo, null);
+		restTemplate.exchange(BASE_URL + port + TODO_API + PATH_SEPARATOR + savedTodo.getId(),
+				HttpMethod.DELETE, requestEntity, TodoVO.class);
+	}
+
+	/**
+	 * Testing for GET /api/todos/{id} endpoint
+	 * NEGATIVE Test Case: Trying to fetch an item which does not exist
+	 */
+	@Test
+	@Order(5)
+	public void getTodo_NonExisting() {
+
+		long expectedTodoId = 99999L;
+		// call GET api to fetch for one
+		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(null, null);
+		ResponseEntity<ErrorVO> responseEntity = restTemplate.exchange(BASE_URL + port + TODO_API + PATH_SEPARATOR + expectedTodoId,
+				HttpMethod.GET, requestEntity, ErrorVO.class);
+
+		ErrorVO errorVo = responseEntity.getBody();
+
+		Assert.isTrue(ErrorCode.TASK_NOT_FOUND.name().equals(errorVo.getErrorCode()), "Expected Todo name=" + ErrorCode.TASK_NOT_FOUND.name() + ". Actual=" + errorVo.getErrorCode());
 	}
 
 	/**
 	 * Testing for POST /api/todos endpoint
 	 */
 	@Test
+	@Order(6)
 	public void createTodo() {
 
 		// create new task to be saved
@@ -78,17 +148,23 @@ class TodoBlApplicationTests {
 				.build();
 
 		// save the new task
-		TodoVO savedTodo = this.restTemplate.postForObject("http://localhost:" + port + "/api/todos", todoToBeSaved, TodoVO.class);
+		TodoVO savedTodo = this.restTemplate.postForObject(BASE_URL + port + TODO_API, todoToBeSaved, TodoVO.class);
 
 		String expectedTodoStatus = "ACTIVE";
 		Assert.isTrue(savedTodo.getName().equals(testTodoName), "Expected Todo name=" + testTodoName + ". Actual=" + savedTodo.getName());
 		Assert.isTrue(savedTodo.getStatus().equals(expectedTodoStatus), "Expected Todo status=" + expectedTodoStatus + ". Actual=" + savedTodo.getStatus());
+
+		// delete after testing
+		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(savedTodo, null);
+		restTemplate.exchange(BASE_URL + port + TODO_API + PATH_SEPARATOR + savedTodo.getId(),
+				HttpMethod.DELETE, requestEntity, TodoVO.class);
 	}
 
 	/**
 	 * Testing for PUT /api/todos/{id} endpoint
 	 */
 	@Test
+	@Order(7)
 	public void updateTodo() {
 
 		// create new task to be saved
@@ -98,11 +174,11 @@ class TodoBlApplicationTests {
 				.build();
 
 		// save the new task
-		TodoVO savedTodo = this.restTemplate.postForObject("http://localhost:" + port + "/api/todos", todoToBeSaved, TodoVO.class);
+		TodoVO savedTodo = this.restTemplate.postForObject(BASE_URL + port + TODO_API, todoToBeSaved, TodoVO.class);
 
 		// get existing task
 		Long expectedTodoId = savedTodo.getId();
-		TodoVO todoToBeUpdated = this.restTemplate.getForObject("http://localhost:" + port + "/api/todos/" + expectedTodoId, TodoVO.class);
+		TodoVO todoToBeUpdated = this.restTemplate.getForObject(BASE_URL + port + TODO_API + PATH_SEPARATOR + expectedTodoId, TodoVO.class);
 
 		String expectedTodoName = "Updated Todo Name";
 		String expectedTodoStatus = "COMPLETED";
@@ -111,25 +187,31 @@ class TodoBlApplicationTests {
 
 		// save task
 		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(todoToBeUpdated, null);
-		ResponseEntity<TodoVO> responseEntity = this.restTemplate.exchange("http://localhost:" + port + "/api/todos/" + expectedTodoId,
+		ResponseEntity<TodoVO> responseEntity = this.restTemplate.exchange(BASE_URL + port + TODO_API + PATH_SEPARATOR + expectedTodoId,
 				HttpMethod.PUT, requestEntity, TodoVO.class, new HashMap<>());
 
 		Assert.isTrue(responseEntity.getStatusCode().equals(HttpStatus.NO_CONTENT),  "Expected HTTP Status=" + HttpStatus.NO_CONTENT.value()
 				+ ". Actual=" + responseEntity.getStatusCode());
 
 		// fetch the task again
-		TodoVO updatedTodo = this.restTemplate.getForObject("http://localhost:" + port + "/api/todos/" + expectedTodoId, TodoVO.class);
+		TodoVO updatedTodo = this.restTemplate.getForObject(BASE_URL + port + TODO_API + PATH_SEPARATOR + expectedTodoId, TodoVO.class);
 
 		// assertions
 		Assert.isTrue(Objects.equals(updatedTodo.getId(), expectedTodoId), "Expected Todo Id=" + expectedTodoId + ". Actual=" + expectedTodoId);
 		Assert.isTrue(updatedTodo.getName().equals(expectedTodoName), "Expected Todo name=" + expectedTodoName + ". Actual=" + todoToBeUpdated.getName());
 		Assert.isTrue(updatedTodo.getStatus().equals(expectedTodoStatus), "Expected Todo status=" + expectedTodoStatus + ". Actual=" + todoToBeUpdated.getStatus());
+
+		// delete after testing
+		requestEntity = new HttpEntity<>(savedTodo, null);
+		restTemplate.exchange(BASE_URL + port + TODO_API + PATH_SEPARATOR + savedTodo.getId(),
+				HttpMethod.DELETE, requestEntity, TodoVO.class);
 	}
 
 	/**
 	 * Testing for DELETE /api/todos/{id} endpoint
 	 */
 	@Test
+	@Order(8)
 	public void deleteTodo() {
 
 		// create new task to be saved
@@ -138,11 +220,11 @@ class TodoBlApplicationTests {
 				.build();
 
 		// save the new task
-		TodoVO savedTodo = this.restTemplate.postForObject("http://localhost:" + port + "/api/todos", todoToBeSaved, TodoVO.class);
+		TodoVO savedTodo = this.restTemplate.postForObject(BASE_URL + port + TODO_API, todoToBeSaved, TodoVO.class);
 
 		// delete the task
 		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(savedTodo, null);
-		ResponseEntity<TodoVO> responseEntity  = restTemplate.exchange("http://localhost:" + port + "/api/todos/" + savedTodo.getId(),
+		ResponseEntity<TodoVO> responseEntity  = restTemplate.exchange(BASE_URL + port + TODO_API + PATH_SEPARATOR + savedTodo.getId(),
 				HttpMethod.DELETE, requestEntity, TodoVO.class);
 
 		// assertions
@@ -154,6 +236,7 @@ class TodoBlApplicationTests {
 	 * Testing for GET /api/todos endpoint
 	 */
 	@Test
+	@Order(100)
 	public void deleteAllTodos() {
 
 		// create new task to be saved
@@ -162,11 +245,11 @@ class TodoBlApplicationTests {
 				.build();
 
 		// save the new task
-		TodoVO savedTodo = this.restTemplate.postForObject("http://localhost:" + port + "/api/todos", todoToBeSaved, TodoVO.class);
+		this.restTemplate.postForObject(BASE_URL + port + TODO_API, todoToBeSaved, TodoVO.class);
 
 		// delete all tasks
 		HttpEntity<TodoVO> requestEntity = new HttpEntity<>(null, null);
-		ResponseEntity<TodoVO> responseEntity  = restTemplate.exchange("http://localhost:" + port + "/api/todos",
+		ResponseEntity<TodoVO> responseEntity  = restTemplate.exchange(BASE_URL + port + TODO_API,
 				HttpMethod.DELETE, requestEntity, TodoVO.class);
 
 		// assertions
@@ -174,8 +257,9 @@ class TodoBlApplicationTests {
 				+ ". Actual=" + responseEntity.getStatusCode());
 
 		// fetch all tasks, they should be 0.
-		List<TodoVO> todos = this.restTemplate.getForObject("http://localhost:" + port + "/api/todos", List.class);
+		List<TodoVO> todos = this.restTemplate.getForObject(BASE_URL + port + TODO_API, List.class);
 		Assert.isTrue(todos.size() == 0, "Expected todos size=0. Actual=" + todos.size());
 	}
+
 
 }
